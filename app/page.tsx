092c66ddd8696a2bc61a2b9e9966f7c795323e1a
Satoki Hata <satokihata@gmail.com>
2024-04-12 22:43:03 +0900

    get blog from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
 import Link from "next/link";
 
-export default function Home() {
+type FetchType = {
+  id: number;
+  title: string;
+  description: string;
+  date: Date;
+};
+
+const fetchAllBlogs = async () => {
+  const res = await fetch(`http://localhost:3000/api/blog`, {
+    cache: "no-store",
+  });
+  const data = await res.json();
+  return data.posts;
+};
+
+export default async function Home() {
+  const posts = await fetchAllBlogs();
   return (
     <main className="w-full h-full">
       <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl">
@@ -19,29 +35,34 @@ export default function Home() {
       </div>
 
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center">
-          <div className="flex items-center my-3">
-            <div className="mr-auto">
-              <h2 className="mr-auto font-semibold">First Post</h2>
+        {posts.map((item: FetchType) => (
+          <div
+            key={item.id}
+            className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center"
+          >
+            <div className="flex items-center my-3">
+              <div className="mr-auto">
+                <h2 className="mr-auto font-semibold">{item.title}</h2>
+              </div>
+              <Link
+                href={`/blog/edit/${item.id}`}
+                className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
+              >
+                編集
+              </Link>
             </div>
-            <Link
-              href={`/blog/edit/1`}
-              className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
-            >
-              編集
-            </Link>
-          </div>
 
-          <div className="mr-auto my-1">
-            <blockquote className="font-bold text-slate-700">
-              2023/8/17
-            </blockquote>
-          </div>
+            <div className="mr-auto my-1">
+              <blockquote className="font-bold text-slate-700">
+                {new Date(item.date).toDateString()}
+              </blockquote>
+            </div>
 
-          <div className="mr-auto my-1">
-            <h2>First Post Description</h2>
+            <div className="mr-auto my-1">
+              <h2>{item.description}</h2>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </main>
   );
