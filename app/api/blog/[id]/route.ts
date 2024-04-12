dd177ff39e5db53eb9120039441caba74f7f2171
Satoki Hata <satokihata@gmail.com>
2024-04-12 12:50:39 +0900

    add api

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -17,3 +17,43 @@ export const GET = async (
     await prisma.$disconnect();
   }
 };
+
+// ブログの詳細記事編集
+export const PUT = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { title, description } = await req.json();
+    const id = parseInt(params.id);
+    await main();
+    const post = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    });
+    return NextResponse.json({ message: "success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+// ブログの詳細記事削除
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const id = parseInt(params.id);
+    await main();
+    const post = await prisma.post.delete({
+      where: { id },
+    });
+    return NextResponse.json({ message: "success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
